17538f00f276754f343a87fb5460212cf0f475d3
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-30 13:28:59 +0800

    github-file-list commit-message

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import PropTypes from 'prop-types';
+import { createRoot } from 'react-dom/client';
+// import Time from './time';
+
+const FileList = ({ files }) => (
+  <table className="file-list">
+    <tbody>
+      {files.map((file) => (
+        <FileListItem key={file.id} file={file} />
+      ))}
+    </tbody>
+  </table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+FileList.propTypes = {
+  files: PropTypes.array,
+};
+
+const FileListItem = ({ file }) => (
+  <tr className="file-list-item">
+    <FileName file={file} />
+    <CommitMessage commit={file.latestCommit} />
+  </tr>
+);
+
+FileListItem.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+function FileIcon({ file }) {
+  let icon = 'fa-file';
+  if (file.type === 'folder') {
+    icon = 'fa-folder';
+  }
+
+  return (
+    <td className="file-icon">
+      <i className={`fa ${icon}`} />
+    </td>
+  );
+}
+
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+function FileName({ file }) {
+  return (
+    <>
+      <FileIcon file={file} />
+      <td className="file-name">{file.name}</td>
+    </>
+  );
+}
+
+FileName.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+const CommitMessage = ({ commit }) => (
+  <td className="commit-message">{commit.message}</td>
+);
+
+CommitMessage.propTypes = {
+  commit: PropTypes.object.isRequired,
+};
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit',
+    },
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: {
+      message: 'Initial commit',
+    },
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: '2016-07-18 21:24:00',
+    latestCommit: {
+      message: 'Added a readme',
+    },
+  },
+];
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<FileList files={testFiles} />);
